21ce1d2664774ec635920c328fdc0ddb3d1182d4
jegehunt <jeffreyhunt@westernwyoming.edu>
2025-01-20 21:23:17 +0000

    Ready to grade, I hope!  Thank you.

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,15 @@
 //
-// Name
-// Date
-// Hello World Programming Project
+// Jeff Hunt
+// 1/20/2025
+// Your First C++ Program
 // COSC 1030
 //
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
 
