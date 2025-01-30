0136c8780998766f40f39fa79d2ab959fbaee666
jegehunt <jeffreyhunt@westernwyoming.edu>
2025-01-30 14:08:08 +0000

    It's ready to be graded! Thank you.

--- restaurant-bill/main.cpp
+++ restaurant-bill/main.cpp
@@ -1,11 +1,31 @@
 //
-// Name
-// Date
+// Jeff Hunt
+// 1/30/25
 // Restaurant Bill Programming Project
 // COSC 1030
 //
 
-// Variable declarations
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variable declarations
+    double meal = 88.67;
+    double tax = meal * 0.0675;
+    double subtotal = meal + tax;
+    double tip = subtotal * 0.15;
+    double total = subtotal + tip;
+
+    cout << "Meal Cost: " << meal << endl;
+    cout << "Tax: "  << tax << endl;
+    cout << "Tip: " << tip << endl;
+    cout << "Total: " << total << endl;
+
+    // Print information about the sale.
+    return 0;
+}
+
 
 // Constants for the tax rate and tip percentage
 
