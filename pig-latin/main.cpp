6153e3e49d456daa9a3fc17d6c07c11d4a2e5dec
jegehunt <jeffreyhunt@westernwyoming.edu>
2025-04-18 01:34:00 +0000

    Ready to grade!

--- pig-latin/main.cpp
+++ pig-latin/main.cpp
@@ -1,7 +1,41 @@
 //
-// Name
-// Date
+// Jeff Hunt
+// 4/17/25
 // Pig Latin Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int SIZE = 80;
+void pigLatin(char const * const );
+int main() {
+
+    char sentence[SIZE], *token;
+    //The user inputs a sentence to be converted
+    cout << "Please enter a sentence:\n";
+    cin.getline(sentence, SIZE);
+
+    cout << "\nPig Latin:\n";
+    token = strtok(sentence, " .,;");
+
+    //Coverts the words to pig latin
+    while (token) {
+pigLatin(token);
+token = strtok(0, " .,;");
+    if (token)
+        cout <<" ";
+    }
+    cout << '.' << endl;
+
+    return 0;
+}
+//Pig Latin Function
+void pigLatin(char const * const word) {
+    int len = strlen(word);
+    for (int i = 1; i < len; ++i)
+        cout << *(word + i);
+        cout << *word << "AY";
+}
