1cef889acce5e4e31782f6039b02923e64154723
jegehunt <jeffreyhunt@westernwyoming.edu>
2025-04-24 22:54:22 +0000

    Ready to grade!  Thank you.

--- string-length/main.cpp
+++ string-length/main.cpp
@@ -1,7 +1,30 @@
 //
-// Name
-// Date
+// Jeff Hunt
+// 4/24/25
 // String Length Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int getChar(const char* str) {
+    int i = 0;
+    while (str[i] !='\0') {
+        i++;
+    }
+    return i;
+}
+
+int main() {
+    string s;
+
+    cout << "Please enter a string: ";
+    cin >> s;
+
+    cout << getChar(s.c_str()) << endl;
+
+    return 0;
+}
+    
 
